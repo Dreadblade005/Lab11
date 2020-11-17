6b8e44b1fe53af9286b8338d55632893cd00983c
dread <dread@DESKTOP-BS1AKPM>
2020-11-16 19:33:01 -0800

    Add project files.

---
+++ part1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace part1
+{   
+    class Program
+    {
+
+    static void Main(string[] args)
+        {
+            person person1 = new person("dakota", 1234567890);
+            person person2 = new person("mark", 0987654321);
+            
+          
+        }
+    }
+}
---
+++ part1/person.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace part1
+{
+    public class person
+    {
+        Dictionary<string, int> people = new Dictionary<string, int>();
+        int number;
+        string name;
+
+        public person(int phone, string person)
+        {
+            people.Add(person, phone);
+        }
+        public person(string person, int phone)
+        {
+            people.Add(person, phone);
+        }
+
+        public void Deletenumber(string name)
+        {
+            if(people.ContainsKey(name))
+            {
+                people.Remove(name);
+            }
+
+        }
+        string Findperson()
+        {
+           string name;
+            Console.WriteLine("Enter the person's name: ");
+            name = Console.ReadLine();
+            if (people.ContainsKey(name))
+            {
+                int value = people[name];
+
+                return "this " + name + " has the phone number: " + value;
+            }
+            else
+            {
+                return "this person does not exist";
+            }
+        }
+
+    }
+}
